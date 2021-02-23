999d2c8561027436f02d89687cd23549bbc59dfc
Pranav <pstatavarti@gmail.com>
2021-02-22 22:43:10 -0800

    Added cd .. and tried to add regular cd and failed

--- shell.py
+++ shell.py
@@ -7,8 +7,13 @@ while not isTerminated:
     actual_time = str(time.strftime("%c", time1))
     promptText = location + " " + actual_time + " ~ $ "
     command = input(promptText)
-    os.system(command)
-    if command == exit:
-        isTerminated = True 
+    if command == "exit":
+        isTerminated = True
+    if command == "cd ..":
+        os.chdir('..')
+        location = os.getcwd()
+    if command[0:1] == "cd":
+        os.chdir(command[3:])
+        location = os.getcwd()
     else:
         os.system(command)
