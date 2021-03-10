d646e2b86609b6ff92fa9e7805edbf1d5c8228d0
Pranav <pstatavarti@gmail.com>
2021-03-10 15:13:08 -0800

    Added bg and fg commands

--- shell.py
+++ shell.py
@@ -4,6 +4,7 @@ import subprocess
 import shlex
 import signal 
 count = 0 
+command_list = []
 isTerminated = False
 while not isTerminated:
     if count == 0:
@@ -27,17 +28,36 @@ while not isTerminated:
     elif command == "pwd":
         os.system("pwd")
     elif command[0:2] == "bg":
-        process_id = command[3:]                            
+        process_id = command[3:]      
+        command_of_process = ""                      
         #want to check if it truly is an existing process
         for x in jobs:
             #does process_id exist in x
             if process_id not in x:
                 print("You entered a process id that doesn't exist")
-            continue
-        os.system("kill -SIGCONT " + process_id)
+        os.kill(process_id, signal.SIGCONT)
+        for y in command_list:
+            if y[1] == process_id:
+                command_of_process = y[0] 
+        args = shlex.split(command_of_process)
+        q = subprocess.Popen(args)
+    elif command[0:2] == "fg":
+        process_id = command[3:]
+        command_of_process = ""
+        for x in jobs:
+            #does process_id exist in x
+            if process_id not in x:
+                print("You entered a process id that doesn't exist")
+        os.kill(process_id, signal.SIGCONT)
+        for y in command_list:
+            if y[1] == process_id:
+                command_of_process = y[0] 
+        args = shlex.split(command_of_process)
+        subprocess.run(args)
     else:                                                                       
         args = shlex.split(command)
         p = subprocess.Popen(args)
         #append process name, and process id 
         jobs.append(command + " | " + str(p.pid))
+        command_list.append((command, p.pid))
     count += 1
